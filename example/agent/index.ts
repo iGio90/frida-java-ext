73aa71032bcc9dcfb9c5fab95a7224dfa3ec4e79
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2019-07-08 04:39:46 +0200

    update example

--- example/agent/index.ts
+++ example/agent/index.ts
@@ -4,14 +4,16 @@ import { JavaExt } from 'frida-java-ext';
  * the args object in callback is an array of crafted objects holding argument data type and value/handle
  */
 
-JavaExt.attachAllMethods('android.app.Activity', (args: any[], method: string, className: string) => {
-    console.log(className, method, JSON.stringify(args));
-});
+Java.performNow(() => {
+    JavaExt.attachAllMethods('android.app.Activity', (args: any[], method: string, className: string) => {
+        console.log(className, method, JSON.stringify(args));
+    });
 
-JavaExt.attachConstructor('android.app.Activity', (args: any[]) => {
-    console.log(JSON.stringify(args));
-});
+    JavaExt.attachConstructor('android.app.Activity', (args: any[]) => {
+        console.log(JSON.stringify(args));
+    });
 
-JavaExt.attachMethod('android.app.Activity', 'onCreate', (args: any[]) => {
-    console.log(JSON.stringify(args));
+    JavaExt.attachMethod('android.app.Activity', 'onCreate', (args: any[]) => {
+        console.log(JSON.stringify(args));
+    });
 });
